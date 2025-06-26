e531cda49c155eab6c309b0460dd536c7bc26cde
Rebeca_Azvdd <rebecazevedo2006@gmail.com>
2025-06-26 13:13:00 -0300

    criando 3 metodos de get by para filtrar itens

---
+++ backend/src/routes/ItensRoutes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { ItensController } from "../controllers/ItensController";
+const itensController = new ItensController();
+
+const itensRouter = Router();
+
+itensRouter.get("/itens", itensController.getAllItensAvailable);
+itensRouter.get("/itens/categoria", itensController.getItensByCategory);
+itensRouter.get("/itens/pesquisa", itensController.getItensByKeyWord);
+
+export { itensRouter };
