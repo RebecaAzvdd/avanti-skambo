23f72b928243dbb0081068d11a85cd47cd8e4994
Rebeca_Azvdd <rebecazevedo2006@gmail.com>
2025-06-28 11:45:18 -0300

    fix: corrigindo e rebase inicial user

--- backend/src/routes/UsersRoutes.js
+++ backend/src/routes/UsersRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Router } from 'express';
 import {UserController} from '../controllers/UserController.js';
 const userController = new UserController();
@@ -8,23 +7,7 @@ const userRouter = Router();
 userRouter.post('/users', userController.createUser);
 userRouter.put('/user/:id', userController.updateUser);
 userRouter.delete('/users/:id', userController.deleteUser);
+userRouter.get('/', UserController.listarTodos);
+userRouter.get('/:id', UserController.buscarPorId);
 
 export {userRouter};
-=======
-// src/routes/usuarioRoutes.js
-
-const { Router } = require('express');
-const UserController = require('../controllers/UserController'); 
-
-const router = Router();
-
-// Define a rota para listar todos os usuários
-// GET /usuarios
-router.get('/', UserController.listarTodos);
-
-// Define a rota para buscar um usuário específico por ID
-// GET /usuarios/:id
-router.get('/:id', UserController.buscarPorId);
-
-module.exports = router;
->>>>>>> feat: implementa endpoints GET e lógica no controller de usuário
