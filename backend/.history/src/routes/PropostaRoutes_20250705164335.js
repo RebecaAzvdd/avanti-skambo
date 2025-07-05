4260ed9e98cbca1f8cc5b7ac1dde84f778d02b2d
VictorSampaioDev <joaovictorsampaio.ti@gmail.com>
2025-07-05 16:57:32 -0300

    criaçao de cancelar proposta

--- backend/.history/src/routes/PropostaRoutes_20250705164335.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { PropostaController } from "../controllers/Propostacontroller.js";
-const propostaController = new PropostaController();
-const propostaRouter = Router();
-
-propostaRouter.patch("/propostas/:id/status", propostaController.updateStatusProposta)
-
-export {propostaRouter};
