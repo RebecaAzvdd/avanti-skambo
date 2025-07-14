8377803314cff86e1a57a80fc7478df6f11ac875
LilianyNunes <lilianynunes10@gmail.com>
2025-07-14 16:00:52 -0300

    feat: estrutura a aplicação com páginas e rotas

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import ItemList from "../components/molecules/item/ItemList"
 import Header from "../components/header/Header"
-
+import Hero from '../components/hero/Hero';
 const Home = () => {
     return (
         <>
         <Header></Header>
+        <Hero/>
         <ItemList></ItemList>
         </>
     )
 }
 
 export default Home;
+
