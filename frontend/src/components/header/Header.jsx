d84232185d255d3fc61c90a4193942b4da543560
unknown <joaovictorsampaio.ti@gmail.com>
2025-07-16 07:56:29 -0300

    mudança na paleta de cores do modal e responsividade

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Header.css';
 import logo from '../../assets/logo-skambo-3.svg';
 import logoName from '../../assets/logo-name-2.svg';
@@ -7,13 +7,14 @@ import menu from '../../assets/menu-burger.svg';
 
 function Header() {
   const [mostrarModal, setMostrarModal] = useState(false);
+  const [pesquisa, setPesquisa] = useState('');
   const modalRef = useRef(null);
+  const navigate = useNavigate();
 
   const toggleModal = () => {
     setMostrarModal((prev) => !prev);
   };
 
-  // Fecha o modal ao clicar fora
   useEffect(() => {
     function handleClickOutside(event) {
       if (modalRef.current && !modalRef.current.contains(event.target)) {
@@ -30,27 +31,37 @@ function Header() {
     };
   }, [mostrarModal]);
 
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log("Pesquisar:", pesquisa);
+  };
+
   return (
     <header className="main-header">
       <div className="logo-container">
-        <button className="menu-button" onClick={toggleModal}>
-          <img src={menu} alt="icone menu" className="menu" />
-        </button>
-        <img src={logo} alt="Logo Skambo" className="logo" />
-        <img src={logoName} alt="Logo Nome Skambo" className="logo-name" />
+        <Link to="/" className="logo-link">
+          <img src={logo} alt="Logo Skambo" className="logo" />
+          <img src={logoName} alt="Logo Nome Skambo" className="logo-name" />
+        </Link>
       </div>
 
       <nav className="header-nav">
         <Link to="/login" className="nav-button login-button">
           Entre ou Cadastre-se
         </Link>
+        <button className="menu-button" onClick={toggleModal}>
+          <img src={menu} alt="icone menu" className="menu" />
+        </button>
       </nav>
 
       {mostrarModal && (
         <div className="modal-menu" ref={modalRef}>
-          <p>🔍 Buscar por itens</p>
-          <p>📦 Categorias</p>
-          <p>📝 Histórico</p>
+         <Link className='filter-link'>
+          <p>Criar item</p>
+         </Link>
+         <Link className='filter-link'>
+          <p>Criar Proposta</p>
+         </Link>
         </div>
       )}
     </header>
