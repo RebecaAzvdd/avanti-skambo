478e6ab6b4cb24a5db96e415ea5135a9614bd285
Rebeca_Azvdd <rebecazevedo2006@gmail.com>
2025-07-16 12:55:27 -0300

    wip: componente de perfil e iniciando logica de auth do header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 function App() {
@@ -9,8 +10,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/login" element={<Login />} />
-           <Route path="/register" element={<Register/>} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
