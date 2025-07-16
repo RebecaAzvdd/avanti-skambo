ebfbd2bb8581d2bc31cab95a817f45c6f2c059b7
Eric James <ericjcf@gmail.com>
2025-07-16 14:04:43 -0300

    commit da login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/LoginPage";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
