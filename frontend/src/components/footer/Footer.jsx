554e18df1f76c3054ddb0a6596ddaf2f50329cc4
matheusousams <sousa.vieira@aluno.uece.br>
2025-07-16 16:31:50 -0300

    criando footer

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <footer className="main-footer">
+            <p>&#169; 2025 Skambo. Todos os direitos reservados.</p>
+        </footer>
+    );
+}
