3b9647461ad34acda042363f8f30367e72ebee6a
unknown <joaovictorsampaio.ti@gmail.com>
2025-07-17 12:15:28 -0300

    mudança no componete filtro

--- frontend/src/components/filterCategory/FilterCategory.jsx
+++ frontend/src/components/filterCategory/FilterCategory.jsx
@@ -32,51 +32,13 @@ export default function FilterCategory() {
         </select>
       </div>
 
-      <div className="filtro-campo">
-        <label>Estado</label>
-        <select>
-          <option>Todos</option>
-          <option>Alagoas</option>
-          <option>Acre</option>
-          <option>Amapá</option>
-          <option>Amazonas</option>
-          <option>Bahia</option>
-          <option>Ceará</option>
-          <option>Brasilia</option>
-          <option>Espírito Santo</option>
-          <option>Goiás</option>
-          <option>Maranhão</option>
-          <option>Mato Grosso</option>
-          <option>Mato Grosso do Sul</option>
-          <option>Minas Gerais</option>
-          <option>Pará</option>
-          <option>Paraíba</option>
-          <option>Paraná</option>
-          <option>Pernambuco</option>
-          <option>Piauí</option>
-          <option>Rio de Janeiro</option>
-          <option>Rio Grande do Norte</option>
-          <option>Rio Grande do Sul</option>
-          <option>Rondônia</option>
-          <option>Roraima</option>
-          <option>Santa Catarina</option>
-          <option>São Paulo</option>
-          <option>Sergipe</option>
-          <option>Tocantins</option>
-        </select>
-      </div>
-
-      <div className="filtro-campo">
-        <label>Condição</label>
-        <select>
-          <option>Todos</option>
-          <option>Novo</option>
-          <option>Usado</option>
-        </select>
+      <div className="filtro-campo filtro-pesquisa">
+        <label>Buscar</label>
+        <input className="texto-input" type="text" placeholder="Pesquise um item..." />
       </div>
 
       <button className="filtro-buscar">
-        <img src={ Search } alt="Pesquisar" />
+        <img src={Search} alt="Pesquisar" />
       </button>
     </div>
   );
