75a2bdbfefc44a29bbaf60895b77e6d3594d23d9
Eric James <ericjcf@gmail.com>
2025-07-18 18:21:53 -0300

    adicionado a estrutura da página de cadastro, validações para tipos e tamanhos dos campos de senha e email, adicionado botão para esconder a senhas, e mudanças mínimas no CSS

--- frontend/src/components/molecules/FormLogin/FormLogin.jsx
+++ frontend/src/components/molecules/FormLogin/FormLogin.jsx
@@ -1,25 +1,70 @@
 import { Link } from "react-router-dom";
 import "./FormLogin.css";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+// npm install react-icons
 
 export default function FormLogin() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const isValidEmail = (email) => {
+      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return regex.test(email);
+    };
+
+    if (!isValidEmail(email)) {
+      alert("Por favor, insira um e-mail válido.");
+      return;
+    }
+
+    if (password.length < 6) {
+      alert("A senha deve conter no mínimo 6 caracteres.");
+      return;
+    }
+
+    console.log("Cadastro efetuado com sucesso! confirmação de dados abaixo: ");
+    console.log({ email, password });
+  };
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="cadastro-container">
-      <form className="cadastro-form">
+      <form className="cadastro-form" onSubmit={handleSubmit}>
         <h1>Login</h1>
         <span>Rápido e grátis, vamos lá!</span>
-
         <input
           name="email"
           label="Email"
           type="email"
           placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
         ></input>
-        <input
-          name="password"
-          type="password"
-          label="Senha"
-          placeholder="password"
-        ></input>
+        <div className="container-input">
+          <input
+            name="password"
+            type={showPassword ? "text" : "password"}
+            label="Senha"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          ></input>
+          <button
+            className="button-senha"
+            type="button"
+            onClick={togglePassword}
+          >
+            {showPassword ? <FaEye size={20} /> : <FaEyeSlash size={20} />}
+          </button>
+        </div>
 
         <button type="submit">Logar</button>
         <span>
