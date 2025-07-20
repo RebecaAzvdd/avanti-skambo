0fee5c4d28b04f1c0b0f8ce0df2f56e714ccab3f
Rebeca_Azvdd <rebecazevedo2006@gmail.com>
2025-07-20 12:42:07 -0300

    feat: endpoint for update user

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,12 +1,35 @@
 import axios from "axios";
-const API_URL = 'http://localhost:3000/api/users';
+const API_URL = "http://localhost:3000/api/users";
 
-export const listarTodos = async() =>{
-    const response = await axios.get(API_URL);
-    return response.data;
+export const listarTodos = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
 };
 
 export const buscarPorId = async (id) => {
   const response = await axios.get(`${API_URL}/${id}`);
   return response.data;
+};
+
+export const updateUser = async (id, nome, email) => {
+  try {
+    const token = localStorage.getItem("token");
+
+    const response = await axios.put(
+      `${API_URL}/${id}`,
+      { nome, email },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    localStorage.setItem("user", JSON.stringify(response.data.user));
+    return response.data.user;
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || "Erro ao atualizar usuário"
+    );
+  }
 };
