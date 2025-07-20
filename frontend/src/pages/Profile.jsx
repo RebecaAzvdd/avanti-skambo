e1c7b23636e3d10fb37d54be9b87a6504bd81fd8
Rebeca_Azvdd <rebecazevedo2006@gmail.com>
2025-07-20 11:56:54 -0300

    wip: propostaList using storage and endpoint and change css from modal

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -3,9 +3,13 @@ import Header from "../components/header/Header";
 import ProfileHeader from "../components/ProfileHeader/ProfileHeader";
 import { logout } from "../controllers/AuthController";
 import { useNavigate } from "react-router-dom";
+import { getPropostaByUserId } from "../services/propostasService";
+import PropostasList from "../components/molecules/PropostasList/PropostasList";
 
 const Profile = () => {
   const [user, setUser] = useState(null);
+  const [propostas, setPropostas] = useState();
+  const [loading, setLoading] = useState()
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -14,7 +18,19 @@ const Profile = () => {
     if (!storedUser) {
       navigate("/login");
     } else {
-      setUser(JSON.parse(storedUser));
+       const parsedUser = JSON.parse(storedUser);
+      setUser(parsedUser);
+
+      getPropostaByUserId(parsedUser.id)
+        .then((data) => {
+          setPropostas(data);
+        })
+        .catch((err) => {
+          console.error("Erro ao buscar propostas:", err);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
     }
   }, [navigate]);
 
@@ -35,6 +51,9 @@ const Profile = () => {
         onLogout={handleLogout}
         onEditAccount={handleEditAccount}
       />
+       {( user &&
+        <PropostasList propostas={propostas} currentUserId={user.id}/>
+       )}
     </>
   );
 };
